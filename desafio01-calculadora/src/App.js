ec6455a8bd03142ee2af9cc873fcc13782990920
Jao-1002 <joaopaulobjj@hotmail.com>
2022-11-23 23:23:02 -0300

    adicionando func nos num e no C

--- desafio01-calculadora/src/App.js
+++ desafio01-calculadora/src/App.js
@@ -14,36 +14,43 @@ import { useState } from 'react'
 const App = () => {
   const [ currentNumbr, setCurrentNumber ] = useState('0');
 
+  const [ firstNumber, setFirstNumber ] = useState('0')
+
   const handleAddNumber = (number) => {
-    setCurrentNumber(prev => `${number}${ prev }`)
+    setCurrentNumber(prev => `${number}${ prev === '0' ? '' : prev }`)
   }
+
+  const handleOnClear = () => {
+    setCurrentNumber('0')
+  }
+
   return (
     <Container>
       <Content>
         <Input value={ currentNumbr } />
         <Row> 
-          <Button label="*"/>
-          <Button label="/"/>
-          <Button label="C"/>
-          <Button label="?"/>
+          <Button label="*" onClick={ () => handleAddNumber('*')}/>
+          <Button label="/" onClick={ () => handleAddNumber('/')}/>
+          <Button label="C" onClick={ () => handleOnClear() }/>
+          <Button label="?" onClick={ () => handleAddNumber('?')}/>
         </Row>
         <Row>
-          <Button label="7"/>
-          <Button label="8"/>
-          <Button label="9"/>
-          <Button label="-"/>
+          <Button label="7" onClick={ () => handleAddNumber('7')}/>
+          <Button label="8" onClick={ () => handleAddNumber('8')}/>
+          <Button label="9" onClick={ () => handleAddNumber('9')}/>
+          <Button label="-" onClick={ () => handleAddNumber('-')}/>
         </Row>
         <Row>
-          <Button label="4"/>
-          <Button label="5"/>
-          <Button label="6"/>
-          <Button label="+"/>
+          <Button label="4" onClick={ () => handleAddNumber('4')}/>
+          <Button label="5" onClick={ () => handleAddNumber('5')}/>
+          <Button label="6" onClick={ () => handleAddNumber('6')}/>
+          <Button label="+" onClick={ () => handleAddNumber('+')}/>
         </Row>
         <Row>
-          <Button label="1" onClick={ () => handleAddNumber('')}/>
-          <Button label="2" onClick={ () => handleAddNumber('')}/>
-          <Button label="3" onClick={ () => handleAddNumber('')}/>
-          <Button label="=" onClick={ () => handleAddNumber('')}/>
+          <Button label="1" onClick={ () => handleAddNumber('1')}/>
+          <Button label="2"  onClick={ () => handleAddNumber('2')}/>
+          <Button label="3" onClick={ () => handleAddNumber('3')}/>
+          <Button label="=" onClick={ () => handleAddNumber('=')}/>
         </Row>
         
         
